84bfcffeae306be8c90bb7ca0cdebce9a848fdff
Flávio Vasconcelos <105315487+eng-flavio@users.noreply.github.com>
2023-01-30 21:58:53 -0300

    Update questaobonus.py

--- Questaobonus/questaobonus.py
+++ Questaobonus/questaobonus.py
@@ -106,15 +106,12 @@ if __name__ == "__main__":
         mlflow.log_param("Epochs", epochs)
         mlflow.log_param("neurons", neurons)
         
+        #Gera os dados de modelo
+        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
         if tracking_url_type_store != "file":
-
-            # Register the model
-            # There are other ways to use the Model Registry, which depends on the use case,
-            # please refer to the doc for more information:
-            # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-            mlflow.sklearn.log_model(lr, "model", registered_model_name="ElasticnetWineModel")
+            mlflow.sklearn.log_model(model, "model", registered_model_name="ElasticnetWineModel")
         else:
-            mlflow.sklearn.log_model(lr, "model")
+            mlflow.sklearn.log_model(model, "model")
 
         #Inicia a interface do MLflow
         subprocess.run(["mlflow", "ui"])
